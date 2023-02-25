bddaf57a41656f83d0351966e0e7de8e6f957e9e
Amritpal Singh <amrit3701@gmail.com>
2023-02-25 17:43:44 +0530

    Updated AppBar

--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -1,11 +1,25 @@
 <template>
   <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      Base Preset
+    <v-app-bar-title>
+      ONDSEL
     </v-app-bar-title>
-  </v-app-bar>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+    </v-app-bar>
 </template>
 
 <script setup>
