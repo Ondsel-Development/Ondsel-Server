8766320f21cbae16daeb85856728eb6204cad770
Amritpal Singh <amrit3701@gmail.com>
2023-03-06 22:54:36 +0530

    frontend: refactored home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,13 @@
 <template>
-<!--  <hello-world />-->
   <ModelViewer />
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
 import ModelViewer from "@/components/ModelViewer";
 
 export default {
   name: 'HomeView',
-  components: { HelloWorld, ModelViewer },
+  components: { ModelViewer },
   data: () => ({
   }),
 }
