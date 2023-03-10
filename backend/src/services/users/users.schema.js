6049b4b591cdc7918c7796e9276a42bb8a7b365c
Amritpal Singh <amrit3701@gmail.com>
2023-03-10 22:35:32 +0530

    backend: added user and authentication service

---
+++ backend/src/services/users/users.schema.js
@@ -0,0 +1,86 @@
+// // For more information about this file see https://dove.feathersjs.com/guides/cli/service.schemas.html
+import { resolve } from '@feathersjs/schema'
+import { Type, getValidator, querySyntax } from '@feathersjs/typebox'
+import { ObjectIdSchema } from '@feathersjs/typebox'
+import { passwordHash } from '@feathersjs/authentication-local'
+import { BadRequest } from '@feathersjs/errors'
+
+import { dataValidator, queryValidator } from '../../validators.js'
+
+// Main data model schema
+export const userSchema = Type.Object(
+  {
+    _id: ObjectIdSchema(),
+    email: Type.String({ format: "email"}),
+    password: Type.Optional(Type.String()),
+    firstName: Type.String(),
+    lastName: Type.String(),
+    createdAt: Type.Number(),
+    updatedAt: Type.Number(),
+  },
+  { $id: 'User', additionalProperties: false }
+)
+export const userValidator = getValidator(userSchema, dataValidator)
+export const userResolver = resolve({
+})
+
+export const userExternalResolver = resolve({
+  // The password should never be visible externally
+  password: async () => undefined,
+})
+
+// Schema for creating new entries
+export const userDataSchema = Type.Pick(userSchema, ['email', 'password', 'firstName', 'lastName'], {
+  $id: 'UserData'
+})
+export const userDataValidator = getValidator(userDataSchema, dataValidator)
+export const userDataResolver = resolve({
+  password: passwordHash({ strategy: 'local' }),
+  createdAt: async () => Date.now(),
+  updatedAt: async () => Date.now(),
+})
+
+// Schema for updating existing entries
+export const userPatchSchema = Type.Partial(userSchema, {
+  $id: 'UserPatch'
+})
+export const userPatchValidator = getValidator(userPatchSchema, dataValidator)
+export const userPatchResolver = resolve({
+  password: passwordHash({ strategy: 'local' }),
+  updatedAt: async () => Date.now(),
+})
+
+// Schema for allowed query properties
+export const userQueryProperties = Type.Pick(userSchema, ['_id', 'email', 'firstName', 'lastName'])
+export const userQuerySchema = Type.Intersect(
+  [
+    querySyntax(userQueryProperties),
+    // Add additional query properties here
+    Type.Object({}, { additionalProperties: false })
+  ],
+  { additionalProperties: false }
+)
+export const userQueryValidator = getValidator(userQuerySchema, queryValidator)
+export const userQueryResolver = resolve({
+  // If there is a user (e.g. with authentication), they are only allowed to see their own data
+  _id: async (value, user, context) => {
+    if (context.params.user) {
+      return context.params.user._id
+    }
+
+    return value
+  }
+})
+
+
+export const uniqueUserValidator = async (context) => {
+  if (context.data.email) {
+    const userService = context.app.service('users');
+    const result = await userService.find({query: {email: context.data.email }});
+    if (result.total > 0) {
+      throw new BadRequest('Invalid Parameters', {
+        errors: { email: 'Email already taken' }
+      })
+    }
+  }
+}
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <v-app>
-    <AppBar />
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
-</template>
+  <v-card>
+    <v-layout>
+      <AppBar />
 
+<!--      <v-navigation-drawer rail location="right" permanent>-->
+<!--          <v-btn icon>-->
+<!--            <v-icon>mdi-magnify</v-icon>-->
+<!--          </v-btn>-->
+<!--      </v-navigation-drawer>-->
+
+      <v-main>
+        <router-view/>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
 
 <script>
 import AppBar from './layouts/default/AppBar';
