8776fc3916bb5834501e00e55169d2a22a85e585
Amritpal Singh <amrit3701@gmail.com>
2023-03-13 23:22:37 +0530

    frontend: updated app-bar, implemented logout

--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -8,20 +8,79 @@
 
     <v-spacer></v-spacer>
 
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
+    <v-btn flat v-if="!loggedInUser && currentRouteName !== 'SignUp'" :to="{ name: 'SignUp' }">
+      SignUp
     </v-btn>
 
-    <v-btn icon>
-      <v-icon>mdi-heart</v-icon>
+    <v-btn flat v-if="!loggedInUser && currentRouteName !== 'Login'" :to="{ name: 'Login' }">
+      Login
     </v-btn>
 
-    <v-btn icon>
-      <v-icon>mdi-dots-vertical</v-icon>
-    </v-btn>
+    <v-menu
+      v-if="loggedInUser"
+      v-model="menu"
+      :close-on-content-click="false"
+      transition="slide-y-transition"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn
+          icon
+          v-bind="props"
+        >
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+
+      <v-card min-width="200">
+        <v-list v-if="loggedInUser">
+          <v-list-item
+            :title="`${loggedInUser.user.firstName} ${loggedInUser.user.lastName}`"
+          >
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            variant="text"
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="primary"
+            variant="text"
+            @click="logout"
+          >
+            Logout
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
+
     </v-app-bar>
 </template>
 
-<script setup>
-  //
+<script>
+import { mapActions, mapState } from 'vuex';
+
+export default {
+  name: 'AppBar',
+
+  data: () => ({
+    menu: false,
+  }),
+  computed: {
+    ...mapState('auth', { loggedInUser: 'payload' }),
+    currentRouteName: (vm) => vm.$route.name
+  },
+  methods: {
+    ...mapActions('auth', {authLogout: 'logout'}),
+    logout() {
+      this.authLogout().then(() => this.$router.push({ name: 'Login' }));
+    },
+  },
+}
 </script>
