ea5358cc959bc96653e0f442d4656147235b3078
Amritpal Singh <amrit3701@gmail.com>
2023-03-18 00:22:53 +0530

    backend: implemented signed url of get upload

--- backend/src/services/upload/upload.class.js
+++ backend/src/services/upload/upload.class.js
@@ -1,21 +1,33 @@
-import AWS from 'aws-sdk';
-import Store from 's3-blob-store';
-import BlobService from 'feathers-blob';
-import multer from 'multer';
-
+import AWS from 'aws-sdk'
+import Store from 's3-blob-store'
+import BlobService from 'feathers-blob'
+import multer from 'multer'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
 
 class UploadService {
-  constructor(options, blobService) {
+  constructor(options, blobService, s3Client) {
     this.options = options;
     this.blobService = blobService;
+    this.s3Client = s3Client;
+  }
+
+  async getSignedFileUrl(fileName, bucket, expiresIn) {
+    const command = new GetObjectCommand({
+      Bucket: bucket,
+      Key: fileName,
+    });
+
+    return await getSignedUrl(this.s3Client, command, { expiresIn });
   }
 
   async get(id, _params) {
-    return await this.blobService.get(id, _params);
+    const url = await this.getSignedFileUrl(id, this.options.app.get('awsClientModelBucket'), 3600)
+    return {url: url};
+
   }
   async create(data, params) {
-    const d = await this.blobService.create(data, params);
-    return d;
+    return await this.blobService.create(data, params);
   }
 
   async remove(id, _params) {
@@ -28,10 +40,13 @@ const getOptions = (app) => {
 }
 
 export const getUploadService = function(app){
-  const s3 = new AWS.S3({
+
+  const credentials = {
     accessKeyId: app.get('awsAccessKeyId'),
-    secretAccessKey: app.get('awsSecretAccessKey')
-  });
+    secretAccessKey: app.get('awsSecretAccessKey'),
+  }
+
+  const s3 = new AWS.S3(credentials);
 
   const blobStore = Store({
     client: s3,
@@ -43,7 +58,12 @@ export const getUploadService = function(app){
     returnUri: false,
   });
 
-  return new UploadService(getOptions(app), blobUploadService);
+  const s3Client = new S3Client({
+    credentials: credentials,
+    region: app.get('awsRegion')
+  });
+
+  return new UploadService(getOptions(app), blobUploadService, s3Client);
 }
 
 
