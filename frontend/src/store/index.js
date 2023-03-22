73158417401fac41c01018ba970a196e37fee70c
Amritpal Singh <amrit3701@gmail.com>
2023-03-22 18:24:54 +0530

    frontend: fixed bug

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,10 @@ import Vuex from 'vuex';
 import auth from './store.auth'
 import user from './services/users';
 import model from './services/models';
-import upload from './services/upload';
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  plugins: [user, auth, model, upload]
+  plugins: [user, auth, model]
 })
