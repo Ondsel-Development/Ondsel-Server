410ef076426b5a704ebf34c1c760df3a3710c98c
Amritpal Singh <amrit3701@gmail.com>
2023-04-16 20:20:20 +0530

    frontend: added store for shared-models service

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,11 @@ import Vuex from 'vuex';
 import auth from './store.auth'
 import user from './services/users';
 import model from './services/models';
+import sharedModel from './services/sharedModel';
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  plugins: [user, auth, model]
+  plugins: [user, auth, model, sharedModel]
 })
