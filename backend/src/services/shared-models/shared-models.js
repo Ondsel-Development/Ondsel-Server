2adb7d6e3baac73dfac355e0d612d37fb579b9bd
Amritpal Singh <amrit3701@gmail.com>
2023-05-28 20:24:03 +0530

    backend: Allow only owner to patch isActive flag

--- backend/src/services/shared-models/shared-models.js
+++ backend/src/services/shared-models/shared-models.js
@@ -111,6 +111,7 @@ export const sharedModels = (app) => {
             'canUpdateModel',
             'canExportModel',
             'dummyModelId',
+            'isActive',
           )
         ),
 
