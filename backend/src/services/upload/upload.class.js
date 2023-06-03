b915e5e1e4c901afa60bf7c8d791d96484db3144
Amritpal Singh <amrit3701@gmail.com>
2023-06-03 22:58:29 +0530

    backend: restrict user to override customer uploaded file (uniqueFileName)

--- backend/src/services/upload/upload.class.js
+++ backend/src/services/upload/upload.class.js
@@ -4,6 +4,7 @@ import BlobService from 'feathers-blob'
 import multer from 'multer'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { GetObjectCommand, S3Client, HeadObjectCommand } from '@aws-sdk/client-s3'
+import { BadRequest } from '@feathersjs/errors'
 
 class UploadService {
   constructor(options, blobService, s3Client) {
@@ -56,6 +57,11 @@ class UploadService {
   }
 
   async create(data, params) {
+    const modelService = this.options.app.service('models');
+    const resp = await modelService.find({ query: { uniqueFileName: data['id'] }});
+    if (resp.data.length) {
+      throw new BadRequest('File already exists!');
+    }
     return await this.blobService.create(data, params);
   }
 
