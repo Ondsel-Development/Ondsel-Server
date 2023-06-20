ef428677e2e655a7563b074ecde8997e72113b47
Amritpal Singh <amrit3701@gmail.com>
2023-06-21 00:30:23 +0530

    backend: #25, allow model API to add fileUpdateAt field

--- backend/src/services/models/models.schema.js
+++ backend/src/services/models/models.schema.js
@@ -16,6 +16,7 @@ export const modelSchema = Type.Object(
     uniqueFileName: Type.String(),
     createdAt: Type.Number(),
     updatedAt: Type.Number(),
+    fileUpdatedAt: Type.Number(),
     isObjGenerationInProgress: Type.Optional(Type.Boolean({default: false})),
     isObjGenerated: Type.Optional(Type.Boolean({default: false})),
     shouldStartObjGeneration: Type.Optional(Type.Boolean()),
@@ -86,6 +87,7 @@ export const modelDataResolver = resolve({
   },
   createdAt: async () => Date.now(),
   updatedAt: async () => Date.now(),
+  fileUpdatedAt: async () => Date.now(),
   // https://github.com/feathersjs/feathers/issues/2837
   isObjGenerationInProgress: async (_value, _message, context) => {
     return modelSchema.properties.isObjGenerationInProgress.default
