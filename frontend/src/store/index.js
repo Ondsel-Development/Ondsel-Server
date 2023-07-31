0ff2b7d620a28790bbc84e7dbbd9cdbb18350f81
Amritpal Singh <amrit3701@gmail.com>
2023-07-31 22:02:00 +0530

    #57, frontend: clear vuex store before login

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,9 +4,18 @@ import user from './services/users';
 import model from './services/models';
 import sharedModel from './services/sharedModel';
 
-export default new Vuex.Store({
+const store =  new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
   plugins: [user, auth, model, sharedModel]
 })
+
+
+export const resetStores = () => {
+  store.commit('users/clearAll');
+  store.commit('models/clearAll');
+  store.commit('shared-models/clearAll');
+};
+
+export default store;
