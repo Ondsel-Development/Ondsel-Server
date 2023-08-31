c1571f689a80eb1b8f73b98693c147755af4a258
John Dupuy <jdupuy98@gmail.com>
2023-08-31 16:39:39 -0500

    normalized markdown and add TOS migration

--- backend/src/migrations/run-migration.js
+++ backend/src/migrations/run-migration.js
@@ -3,6 +3,7 @@ import { migrateOldModelsCommand } from './migrate-models.command.js';
 import {updateTierNames} from "./update-tier-names.command.js";
 import {addInitialTosPp} from "./add-initial-tos-pp.js";
 import { migrateOldFilesCommand } from './migrate-old-files.command.js';
+import {updateTos2023Aug31Command} from "./update-tos-2023-aug-31.command.js";
 
 async function runMigration() {
   console.log('Migration start');
@@ -14,11 +15,14 @@ async function runMigration() {
     case 'migrateOldFiles':
       await migrateOldFilesCommand(app);
       break;
-    case 'updateTierNames':
-      await updateTierNames(app);
-      break;
-    case 'addInitialTosPp':
-      await addInitialTosPp(app);
+    // case 'updateTierNames':
+    //   await updateTierNames(app);
+    //   break;
+    // case 'addInitialTosPp':
+    //   await addInitialTosPp(app);
+    //   break;
+    case 'updateTos2023Aug31':
+      await updateTos2023Aug31Command(app);
       break;
     default:
       console.error('Please specify the migration command.')
--- frontend/src/views/LegalDoc.vue
+++ frontend/src/views/LegalDoc.vue
@@ -1,48 +1,50 @@
 <template>
-  <v-card
-    v-if="legit_category"
-    variant="outlined"
-  >
-    <v-card-subtitle>ver {{ specific_doc.version }}</v-card-subtitle>
-    <v-card-text>
-      <div v-html='markdown'></div>
-    </v-card-text>
-  </v-card>
-  <v-card
-    v-else
-    variant="outlined"
-  >
+  <div class="pa-3">
+    <v-card
+      v-if="legit_category"
+      variant="outlined"
+    >
+      <v-card-subtitle>ver {{ specific_doc.version }}</v-card-subtitle>
       <v-card-text>
-        There does not appear to be a legal document called '{{ category_name }}'.
+        <div v-html='markdown' class="pa-3"></div>
       </v-card-text>
-  </v-card>
-  <v-divider></v-divider>
-  <div v-if="legit_category">
-    <h2>History</h2>
-    <v-table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Version</th>
-          <th>Effective</th>
-          <th>Deprecated</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in history">
-          <td>{{item.title}}</td>
-          <td>{{item.version}}</td>
-          <td>{{new Date(item.effective).toUTCString()}}</td>
-          <td>{{item.deprecated ? new Date(item.deprecated).toUTCString() : "active"}}</td>
-        </tr>
-      </tbody>
-    </v-table>
+    </v-card>
+    <v-card
+      v-else
+      variant="outlined"
+    >
+        <v-card-text>
+          There does not appear to be a legal document called '{{ category_name }}'.
+        </v-card-text>
+    </v-card>
+    <v-divider></v-divider>
+    <div v-if="legit_category">
+      <h2>History</h2>
+      <v-table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Version</th>
+            <th>Effective</th>
+            <th>Deprecated</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in history" :key="item.version">
+            <td>{{item.title}}</td>
+            <td>{{item.version}}</td>
+            <td>{{new Date(item.effective).toUTCString()}}</td>
+            <td>{{item.deprecated ? new Date(item.deprecated).toUTCString() : "active"}}</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </div>
   </div>
 </template>
 
 <script>
 import {models} from "@feathersjs/vuex";
-import nmd from "nano-markdown";
+import {marked} from "marked";
 
 export default {
   name: 'LegalDoc',
@@ -68,7 +70,7 @@ export default {
       const agreement = (response.data.length > 0) ? response.data[0] : {current:{markdownContent: 'doc missing'}};
       this.specific_doc = agreement.current;
       this.history = agreement.history ? agreement.history : [];
-      this.markdown =  nmd(this.specific_doc.markdownContent);
+      this.markdown =  marked.parse(this.specific_doc.markdownContent);
     });
   },
 }
