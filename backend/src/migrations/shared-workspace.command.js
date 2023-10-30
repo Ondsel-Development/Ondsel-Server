b9e94626d536942a886841359782afc6e9dd45c2
Amritpal Singh <amrit3701@gmail.com>
2023-10-30 23:48:27 +0530

    backend: patching files to rootDirectory

--- backend/src/migrations/shared-workspace.command.js
+++ backend/src/migrations/shared-workspace.command.js
@@ -5,6 +5,7 @@ export async function migrateObjectsForSharedWorkspaceCommand(app) {
   const userService = app.service('users');
   const organizationService = app.service('organizations');
   const workspaceService = app.service('workspaces');
+  const directoryService = app.service('directories');
   const fileService = app.service('file');
   const users = await userService.find({ paginate: false });
   for (let user of users) {
@@ -20,7 +21,7 @@ export async function migrateObjectsForSharedWorkspaceCommand(app) {
     for (let file of files) {
       console.log(`-- patching file (id: ${file._id.toString()})`);
       await fileService.patch(
-        file._id.toString(),
+        file._id,
         {
           workspace: _.pick(workspace, ['_id', 'name']),
           directory: workspace.rootDirectory,
@@ -28,5 +29,15 @@ export async function migrateObjectsForSharedWorkspaceCommand(app) {
         { user: user }
       );
     }
+
+    await directoryService.patch(
+      workspace.rootDirectory._id,
+      {
+        shouldAddFilesToDirectory: true,
+        fileIds: files.map(file => file._id)
+      },
+      { user: user }
+    );
+    console.log(`-- patching adding files to root directory (id: ${workspace.rootDirectory._id.toString()})`);
   }
 }
