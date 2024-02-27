d219b72295a592fa7553335aa1aae7f778233f62
John Dupuy <jdupuy98@gmail.com>
2024-02-27 12:59:29 -0600

    trivia: one less file api query on private workspaces

--- frontend/src/views/WorkspaceHome.vue
+++ frontend/src/views/WorkspaceHome.vue
@@ -259,7 +259,9 @@ export default {
       'getFileByIdPublic',
     ]),
     async clickedFile(fileSubDocs, filePath) {
-      let file = await this.getFileByIdPublic(fileSubDocs._id);
+      let file = this.publicView
+        ? await this.getFileByIdPublic(fileSubDocs._id)
+        : File.getFromStore(fileSubDocs._id);
       if (!file) {
         await File.get(fileSubDocs._id);
         file = File.getFromStore(fileSubDocs._id);
