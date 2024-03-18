e8b218be67478d85b01ef287ad54729b007cff71
John Dupuy <jdupuy98@gmail.com>
2024-03-18 13:38:47 -0500

    plain lens home page built

--- frontend/src/views/LensHome.vue
+++ frontend/src/views/LensHome.vue
@@ -1,42 +1,41 @@
 <template>
   <v-container>
-    <v-row class="pl-4 pr-4">
-      <div class="text-h5">LENS</div>
-      <v-spacer />
-    </v-row>
-    <br>
-    <v-row class="pl-4 pr-4">
-      Lens is a system of storing, sharing, and collaborating on CAD models.
-      <v-spacer />
-    </v-row>
+    <v-card>
+      <v-card-title>LENS</v-card-title>
+      <v-card-text>
+        <div v-html="markdownHtml"></div>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
 <script>
-import {mapGetters, mapState} from "vuex";
+
+import {models} from "@feathersjs/vuex";
+import {marked} from "marked";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'LensHome',
   components: { },
   data: () => ({
+    lensSiteCuration: {},
+    markdownHtml: 'missing data',
   }),
   async created() {
-    if (this.userCurrentOrganization) {
-      if (this.userCurrentOrganization.type === 'Personal') {
-        this.$router.push({ name: 'UserWorkspaces', params: { id: this.user.username } });
-      } else {
-        this.$router.push({ name: 'OrganizationWorkspaces', params: { id: this.userCurrentOrganization.refName } });
+    models.api.Agreements.find({
+      query: {category: 'lens-site-curation'}
+    }).then(response => {
+      if (response.data.length > 0) {
+        this.lensSiteCuration = response.data[0];
+        this.markdownHtml =  marked.parse(this.lensSiteCuration.current.markdownContent);
+        console.log(this.lensSiteCuration);
       }
-    } else {
-      this.$router.push({ name: 'UserWorkspaces', params: { id: this.user.username } });
-    }
+    });
   },
   async mounted() {
   },
   computed: {
-    ...mapState('auth', ['user']),
-    ...mapGetters('app', { userCurrentOrganization: 'currentOrganization' }),
   },
   methods: {
   }
