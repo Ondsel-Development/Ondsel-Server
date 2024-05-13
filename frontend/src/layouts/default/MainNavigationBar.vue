e0b3c4847e4f42c14379064db98fc5e14a15cce2
Amritpal Singh <amrit3701@gmail.com>
2024-05-13 21:53:01 +0530

    LENS-169: Fixed initials

--- frontend/src/layouts/default/MainNavigationBar.vue
+++ frontend/src/layouts/default/MainNavigationBar.vue
@@ -260,6 +260,9 @@ export default {
       const nameArray = name.split(' ');
       const firstName = nameArray[0].charAt(0).toUpperCase();
       const lastName = nameArray[nameArray.length - 1].charAt(0).toUpperCase();
+      if (nameArray.length === 1) {
+        return firstName;
+      }
       return firstName + lastName;
     },
   },
