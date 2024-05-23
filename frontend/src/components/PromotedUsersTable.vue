8f09f0ef05588c6e0fc5d3e1b09e51ef4f15c5a4
Amritpal Singh <amrit3701@gmail.com>
2024-05-24 00:53:32 +0530

    LENS-170: User table for promoted users

---
+++ frontend/src/components/PromotedUsersTable.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="promotedUsers"
+    hover
+    item-selectable
+  >
+    <template v-slot:item="{ item }">
+      <tr @click="$router.push({ name: 'UserHome', params: { slug: item.curation.nav.username } })">
+        <td>
+          <div class="d-flex flex-row align-center">
+            <v-sheet class="d-flex flex-column justify-center align-center text-uppercase ma-1" width="25" height="25" rounded="circle" color="grey">
+              {{ getInitials(item.curation.name) }}
+            </v-sheet>
+            {{ item.curation.name }}
+          </div>
+        </td>
+        <td>{{ item.curation.description }}</td>
+        <td>{{ item.curation.message }}</td>
+        <td><v-btn icon="mdi-arrow-right" flat @click.stop="goToUserProfile(item)"></v-btn></td>
+      </tr>
+    </template>
+
+  </v-data-table>
+</template>
+
+<script>
+import { getInitials } from '@/genericHelpers';
+
+export default {
+  name: "PromotedUsersTable",
+  props: {
+    promotedUsers: Array,
+  },
+  data: () => ({
+    headers: [
+      {
+        title: 'Name',
+        align: 'start',
+        sortable: true,
+      },
+      {
+        title: 'Description',
+        align: 'start',
+        sortable: false,
+      },
+      {
+        title: 'Message',
+        align: 'start',
+        sortable: false,
+      },
+      {
+        title: 'Action',
+        align: 'start',
+        sortable: false,
+      },
+    ]
+  }),
+  methods: {
+    getInitials,
+    goToUserProfile(item) {
+      this.$router.push({ name: 'UserHome', params: { slug: item.curation.nav.username } });
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
