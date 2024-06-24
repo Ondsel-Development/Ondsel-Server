4d3a03a55f73b761796e8d822c34d07b0d2cba11
John Dupuy <jdupuy98@gmail.com>
2024-06-23 19:46:21 -0500

    a post-create patch does not play well with feathers

--- backend/src/services/shared-models/shared-models.curation.js
+++ backend/src/services/shared-models/shared-models.curation.js
@@ -25,11 +25,18 @@ export function buildNewCurationForSharedModel(sm) {
 export const afterCreateHandleSharedModelCuration = async (context) => {
   // first, set up the curation
   context.result.curation = buildNewCurationForSharedModel(context.result);
-  await context.service.patch(
-    context.result._id,
+  console.log("starting patch");
+  const smService = context.service;
+  const smDb = await smService.options.Model;
+  await smDb.updateOne(
+    { _id: context.result._id },
     {
-      curation: context.result.curation
+      $set: {
+        curation: context.result.curation
+      },
     }
   )
+  console.log(context.result.curation);
+  console.log("done with patch");
   return context;
 }
