863731c2c760d7b3c65c264c3f5de5b8b6f955f3
Amritpal Singh <amrit3701@gmail.com>
2024-07-02 11:58:54 +0530

    Fix: Handle '/public/<id>' in /upload endpoint

--- backend/src/services/upload/upload.class.js
+++ backend/src/services/upload/upload.class.js
@@ -75,6 +75,7 @@ class UploadService {
   }
 
   async get(id, _params) {
+    id = decodeURIComponent(id);
     const bucketName = this.options.app.get('awsClientModelBucket');
     const isFileExist = await this.checkFileExists(bucketName, id);
 
