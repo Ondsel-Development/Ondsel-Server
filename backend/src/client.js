dcf54b459c88d8bf2d95a1e0248e1c7143343918
John Dupuy <jdupuy98@gmail.com>
2024-10-22 16:52:07 -0500

    Added Stripe Events webhook

--- backend/src/client.js
+++ backend/src/client.js
@@ -1,6 +1,8 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/client.html
 import { feathers } from '@feathersjs/feathers'
 import authenticationClient from '@feathersjs/authentication-client'
+import { stripeEventsClient } from './services/stripe-events/stripe-events.shared.js'
+
 import { userEngagementsClient } from './services/user-engagements/user-engagements.shared.js'
 
 import { notificationsClient } from './services/notifications/notifications.shared.js'
@@ -107,6 +109,8 @@ export const createClient = (connection, authenticationOptions = {}) => {
   client.configure(downloadClient)
 
   client.configure(publisherClient)
+
+  client.configure(stripeEventsClient)
 
   return client
 }
---
+++ backend/src/services/stripe-events/stripe-events.js
@@ -0,0 +1,61 @@
+// For more information about this file see https://dove.feathersjs.com/guides/cli/service.html
+
+import { hooks as schemaHooks } from '@feathersjs/schema'
+import {
+  stripeEventsDataValidator,
+  stripeEventsPatchValidator,
+  stripeEventsQueryValidator,
+  stripeEventsResolver,
+  stripeEventsExternalResolver,
+  stripeEventsDataResolver,
+  stripeEventsPatchResolver,
+  stripeEventsQueryResolver
+} from './stripe-events.schema.js'
+import { StripeEventsService, getOptions } from './stripe-events.class.js'
+import { stripeEventsPath, stripeEventsMethods } from './stripe-events.shared.js'
+
+export * from './stripe-events.class.js'
+export * from './stripe-events.schema.js'
+
+// A configure function that registers the service and its hooks via `app.configure`
+export const stripeEvents = (app) => {
+  // Register our service on the Feathers application
+  app.use(stripeEventsPath, new StripeEventsService(getOptions(app)), {
+    // A list of all methods this service exposes externally
+    methods: stripeEventsMethods,
+    // You can add additional custom events to be sent to clients here
+    events: []
+  })
+  // Initialize hooks
+  app.service(stripeEventsPath).hooks({
+    around: {
+      all: [
+        schemaHooks.resolveExternal(stripeEventsExternalResolver),
+        schemaHooks.resolveResult(stripeEventsResolver)
+      ]
+    },
+    before: {
+      all: [
+        schemaHooks.validateQuery(stripeEventsQueryValidator),
+        schemaHooks.resolveQuery(stripeEventsQueryResolver)
+      ],
+      find: [],
+      get: [],
+      create: [
+        schemaHooks.validateData(stripeEventsDataValidator),
+        schemaHooks.resolveData(stripeEventsDataResolver)
+      ],
+      patch: [
+        schemaHooks.validateData(stripeEventsPatchValidator),
+        schemaHooks.resolveData(stripeEventsPatchResolver)
+      ],
+      remove: []
+    },
+    after: {
+      all: []
+    },
+    error: {
+      all: []
+    }
+  })
+}
