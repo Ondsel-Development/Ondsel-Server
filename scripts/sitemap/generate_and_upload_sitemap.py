42849a540381041ae06fd08013b2bc19686acc41
Pieter Hijma <pieterhijma@users.noreply.github.com>
2024-11-22 19:10:21 +0100

    Add license information scripts and root files

--- scripts/sitemap/generate_and_upload_sitemap.py
+++ scripts/sitemap/generate_and_upload_sitemap.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 Ondsel <development@ondsel.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 import os
 from datetime import datetime
 from xml.etree.ElementTree import Element, SubElement, tostring, ElementTree
